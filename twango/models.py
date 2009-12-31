52ca5c23b8fc41fa0f1c741adc41768a8aad7eb3
Joshua <jroesslein@gmail.com>
2009-12-31 00:37:23 -0600

    Added abstract models 'User' and 'UserProfile'.

--- twango/models.py
+++ twango/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """Twitter user profile infomation"""
+
+    class Meta:
+        abstract = True
+
+    profile_image_url = models.URLField(verify_exists=False)
+    background_image_url = models.URLField(verify_exists=False)
+    background_tile = models.BooleanField()
+    background_color = models.CharField(max_length=6)
+    text_color = models.CharField(max_length=6)
+    link_color = models.CharField(max_length=6)
+    sidebar_fill_color = models.CharField(max_length=6)
+    sidebar_border_color = models.CharField(max_length=6)
+
+
+class User(models.Model):
+    """Twitter user infomation"""
+
+    class Meta:
+        abstract = True
+
+    twitter_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=20)
+    screen_name = models.CharField(max_length=15)
+    location = models.CharField(max_length=30)
+    description = models.CharField(max_length=160)
+    url = models.URLField(verify_exists=False)
+    protected = models.BooleanField()
+    followers_count = models.PositiveIntegerField()
+    created_at = models.DateTimeField()
+    favourites_count = models.PositiveIntegerField()
+    utc_offset = models.IntegerField()
+    time_zone = models.CharField(max_length=50)
+    statuses_count = models.PositiveIntegerField()
+    verified = models.BooleanField()
+
