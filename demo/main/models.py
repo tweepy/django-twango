5f56f22b5e49703046b92bcebc1c3bf5fa50fe68
Joshua <jroesslein@gmail.com>
2009-12-31 00:49:17 -0600

    Update demo.

---
+++ demo/main/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from twango.models import Status
+
+
+class DemoStatus(Status):
+    """Extend twango status model"""
+
--- demo/settings.py
+++ demo/settings.py
@@ -76,5 +76,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'main',
     'twango',
 )
